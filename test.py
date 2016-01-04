b140a0cc4af9eaf51c313200a093e0fcf8afffd7
Luke Miles <rashreportloaf@gmail.com>
2016-01-04 18:03:47 -0500

    working click growth game

--- test.py
+++ test.py
@@ -5,15 +5,32 @@ from pygame.locals import *
 pygame.init()
 fpsClock = pygame.time.Clock()
 
+mousex, mousey = 0, 0
 WIDTH = 640
 HEIGHT = 480
 
+circleRadius = 50.0
+
 windowSurfaceObj = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Physics Test")
-pygame.draw.circle(windowSurfaceObj, (255,0,0), (WIDTH/2, HEIGHT/2)
 
 while True:
+    windowSurfaceObj.fill((0, 0, 0))
+
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
+        elif event.type == MOUSEMOTION:
+            mousex, mousey = event.pos
+        elif event.type == MOUSEBUTTONUP:
+            if event.button == 1:
+                circleRadius += 10
+
+    pygame.draw.circle(windowSurfaceObj, (255,0,0), (mousex, mousey),
+                       int(circleRadius))
+    pygame.display.update()
+
+    circleRadius *= 0.99
+
+    fpsClock.tick(30)
