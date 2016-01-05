f6c4cebf9ce4e7e078118a2addd87fa2b45257ed
Luke Miles <rashreportloaf@gmail.com>
2016-01-05 03:08:44 -0500

    made balls into classes, wasd key controls, stable version

--- test.py
+++ test.py
@@ -1,35 +1,47 @@
+# todo:
+#  -finalize code and commit
+#  -add in full map feature
 import pygame, sys
 from pygame.locals import *
+from ball import Ball
+import pdb
 
-pygame.init()
-fpsClock = pygame.time.Clock()
-
-mousex, mousey = 0, 0
 WIDTH = 640
 HEIGHT = 480
 
-circleRadius = min(width, height)/2.0
+myBall = Ball(min(WIDTH, HEIGHT)/8.0, (WIDTH//2, HEIGHT//2))
 
+pygame.init()
+fpsClock = pygame.time.Clock()
 windowSurfaceObj = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Physics Test")
+pygame.display.set_caption("Physics Fun")
 
 while True:
-    windowSurfaceObj.fill((0,0,0))
-
-    pygame.draw.circle(windowSurfaceObj, (255,0,0), (mousex, mousey),
-                       int(circleRadius))
 
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
-        elif event.type == MOUSEMOTION:
-            mousex, mousey = event.pos
-        elif event.type == MOUSEBUTTONUP:
-            if event.button == 1:
-                circleRadius += 10
+        elif event.type == KEYDOWN:
+            if event.key == K_p:
+                pdb.set_trace()
 
-    circleRadius *= 0.99
+    keyValues = pygame.key.get_pressed()
+    # get wasd keys
+    if keyValues[K_w]:
+        myBall.yv -= 1
+    if keyValues[K_s]:
+        myBall.yv += 1
+    if keyValues[K_a]:
+        myBall.xv -= 1
+    if keyValues[K_d]:
+        myBall.xv += 1
+
+    myBall.move()
+
+    windowSurfaceObj.fill((0,0,0))
+    pygame.draw.circle(windowSurfaceObj, (255,0,0),
+            (int(myBall.x) % WIDTH, int(myBall.y) % HEIGHT), int(myBall.r))
     pygame.display.update()
 
     fpsClock.tick(30)
